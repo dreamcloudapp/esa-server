32a917d8bc3fb023bb14d7e124a072428ba64977
Sheldon Juncker <sheldonjuncker@gmail.com>
2022-02-01 18:39:13 -0500

    Adding document names to similarity info.

--- src/main/java/com/dreamcloud/esa_server/DocumentSimilarityScorer.java
+++ src/main/java/com/dreamcloud/esa_server/DocumentSimilarityScorer.java
@@ -3,6 +3,9 @@ package com.dreamcloud.esa_server;
 import com.dreamcloud.esa_core.similarity.DocumentSimilarity;
 import com.dreamcloud.esa_core.similarity.SimilarityInfo;
 import com.dreamcloud.esa_core.vectorizer.TextVectorizer;
+import com.dreamcloud.esa_score.score.DocumentNameResolver;
+
+import java.util.ArrayList;
 
 public class DocumentSimilarityScorer {
     TextVectorizer vectorizer;
@@ -15,6 +18,10 @@ public class DocumentSimilarityScorer {
     public DocumentScore score(DocumentSimilarityRequestBody request) throws Exception {
         DocumentSimilarity similarity = new DocumentSimilarity(vectorizer);
         SimilarityInfo similarityInfo = similarity.score(request.documentText1, request.documentText2, true);
-        return new DocumentScore("success", similarityInfo.getScore(), similarityInfo.getTopConcepts());
+        ArrayList<String> parsedTopConcepts = new ArrayList<>();
+        for(String topConcept: similarityInfo.getTopConcepts()) {
+            parsedTopConcepts.add(DocumentNameResolver.getTitle(Integer.parseInt(topConcept)));
+        }
+        return new DocumentScore("success", similarityInfo.getScore(), parsedTopConcepts);
     }
 }
